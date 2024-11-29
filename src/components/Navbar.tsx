74258220c1d5b6d4a85fd15114f17f2872ad437f
john ennis <john.ennis@livetilesglobal.com>
2024-11-29 11:03:05 +0000

    Added base navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppBar } from '@mui/material';
+
+
+const Navbar: React.FC = () => {
+    return (
+        <AppBar>
+            <h1>Giffer</h1>
+        </AppBar>
+    )
+}
+
+export default Navbar;
